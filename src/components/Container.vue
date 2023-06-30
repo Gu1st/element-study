e6d7fe8b266a2b00524b655246cc9e5908dd0859
Harexs <858333412@qq.com>
2023-06-30 12:02:38 +0800

    init package

---
+++ src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "HarexsContainer",
+});
+const props = defineProps({
+  direction: {
+    type: String,
+  },
+});
+console.log(props);
+</script>
